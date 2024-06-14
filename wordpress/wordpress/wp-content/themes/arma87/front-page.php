da490494bc7952bf0f09cc5321b5884fe57e3ecb
arma <montymo87@gmail.com>
2024-06-14 15:22:15 +0300

    feat base theme

---
+++ wordpress/wordpress/wp-content/themes/arma87/front-page.php
@@ -0,0 +1,9 @@
+<?php
+get_header();
+
+get_template_part('template-parts/hero');
+get_template_part('template-parts/device');
+get_template_part('template-parts/clients');
+get_template_part('template-parts/contact');
+
+get_footer();
